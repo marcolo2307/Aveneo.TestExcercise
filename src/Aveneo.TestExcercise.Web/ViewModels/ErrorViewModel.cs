e2e44890f5616c64083d860acde6ac28af67bc07
Marek Przydanek <marcolo2307@gmail.com>
2019-05-06 19:56:16 +0200

    Add Home controller

---
+++ src/Aveneo.TestExcercise.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Aveneo.TestExcercise.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
