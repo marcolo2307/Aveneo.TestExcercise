73b844c9ef7f32e343302b1e9756dd8d39cd14de
Marek Przydanek <marcolo2307@gmail.com>
2019-05-08 23:20:06 +0200

    Add DataObjectService

---
+++ src/Aveneo.TestExcercise.ApplicationCore/Services/IDataObjectService.cs
@@ -0,0 +1,12 @@
+﻿using Aveneo.TestExcercise.ApplicationCore.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Aveneo.TestExcercise.ApplicationCore.Services
+{
+    public interface IDataObjectService
+    {
+        Task<ICollection<Feature>> GetFeatures(DataObject dataObject);
+        Task SetFeatures(DataObject dataObject, IEnumerable<Feature> features);
+    }
+}
---
+++ src/Aveneo.TestExcercise.ApplicationCore/Services/Implementations/DataObjectService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aveneo.TestExcercise.ApplicationCore.Entities;
+
+namespace Aveneo.TestExcercise.ApplicationCore.Services.Implementations
+{
+    public class DataObjectService : IDataObjectService
+    {
+        private IRepository<Feature> _features { get; }
+        private IRepository<DataObjectFeature> _dataObjectFeatures { get; }
+
+        public DataObjectService(
+            IRepository<Feature> features,
+            IRepository<DataObjectFeature> dataObjectFeatures)
+        {
+            _features = features;
+            _dataObjectFeatures = dataObjectFeatures;
+        }
+
+        public async Task<ICollection<Feature>> GetFeatures(DataObject dataObject)
+        {
+            var dataObjectFeatures = await _dataObjectFeatures
+                .WhereAsync(dof => dof.DataObjectId == dataObject.Id);
+
+            var featuresIds = dataObjectFeatures.Select(dof => dof.FeatureId);
+
+            var features = await _features.WhereAsync(f => featuresIds.Contains(f.Id));
+
+            return features;
+        }
+
+        public async Task SetFeatures(DataObject dataObject, IEnumerable<Feature> features)
+        {
+            var exitstingBindings = await _dataObjectFeatures
+                .WhereAsync(dof => dof.DataObjectId == dataObject.Id);
+            await _dataObjectFeatures.DeleteAsync(exitstingBindings);
+
+            var bindings = features.ToList().ConvertAll<DataObjectFeature>(f => new DataObjectFeature
+            {
+                DataObjectId = dataObject.Id,
+                FeatureId = f.Id
+            });
+
+            await _dataObjectFeatures.CreateAsync(bindings);
+        }
+    }
+}
