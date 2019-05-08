2ebbaeaac81403fa8cde5fc5390a7ff81fb83055
Marek Przydanek <marcolo2307@gmail.com>
2019-05-08 23:11:10 +0200

    Add Init migration

---
+++ src/Aveneo.TestExcercise.Infrastructure/Data/Migrations/20190508210911_Init.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aveneo.TestExcercise.Infrastructure.Data.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataObjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(19,4)", nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Location_Latitude = table.Column<decimal>(type: "decimal(19,4)", nullable: false),
+                    Location_Longitude = table.Column<decimal>(type: "decimal(19,4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataObjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DateObjectFeatures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DataObjectId = table.Column<int>(nullable: false),
+                    FeatureId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DateObjectFeatures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IconName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataObjects");
+
+            migrationBuilder.DropTable(
+                name: "DateObjectFeatures");
+
+            migrationBuilder.DropTable(
+                name: "Features");
+        }
+    }
+}
